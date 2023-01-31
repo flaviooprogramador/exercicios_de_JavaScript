d3f9eecc9f9c2bb8a18ef394970ad591bec18cac
Flavio de oliveira <flaviogamer7007@gmail.com>
2023-01-30 23:09:16 -0300

    validador de email

--- validador_de_email_e_senha/index.js
+++ validador_de_email_e_senha/index.js
@@ -1,5 +1,36 @@
 function validateEmail(email){
   if (!email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/)){
-    const err = new Error()
+    const err = new Error('Email inválido')
+    err.input = ('email')
+    throw err
   }
 }
+
+function resetFormstyles(inputs){ 
+  Object.entries(inputs).forEach(([key,value]) => {
+    value.classList.remove('sucess', 'error')
+    document.querySelector(`#${key}-error`).textContent = ''
+  })
+}
+
+const userInputs = {
+  name: document.querySelector('#name'),
+  email: document.querySelector('#email'),
+  password: document.querySelector('#password')
+}
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (ev) =>{
+  ev.preventDefault()
+  resetFormstyles(userInputs)
+
+  try {
+    userInputs.name.classList.add('success')
+    validateEmail(userInputs.email.value)
+    userInputs.email.classList.add('sucess')
+  } catch (err) {
+    userInputs[err.input].classList.add('error')
+    document.querySelector(`#${err.input}-error`).textContent = err.message
+  }
+})
