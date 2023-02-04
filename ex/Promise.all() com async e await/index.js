c9b32fc0361c65d7e6981261d96ca1b9861a22ca
Flavio de oliveira <flaviogamer7007@gmail.com>
2023-02-04 18:28:58 -0300

    Promise.all()
     Já vimos que podemos utilizar funções async dentro do Promise.all(), afinal elas retornam uma promise. Porém, como o próprio Promise.all() retorna uma promise também podemos utilizar o await com ele. No código abaixo

---
+++ ex/Promise.all() com async e await/index.js
@@ -0,0 +1,29 @@
+//1. Já vimos que podemos utilizar funções async dentro do Promise.all(), afinal elas retornam uma promise. Porém, como o próprio Promise.all() retorna uma promise também podemos utilizar o await com ele. No código abaixo vemos que a função .map() está retornando um array de promises pendentes:
+    
+//Obs.: a função waitFor() é apenas uma forma simples de fazer o código esperar por uma quantidade arbitrária de tempo, assim simulamos um código que demora a ser executado.
+
+function waitFor(seconds){
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, seconds * 1000)
+  })
+}
+
+const numbers = [4,5,9,13, 77]
+
+async function execute(){
+  console.time('map')
+
+  const squares = await Promise.all(numbers.map(async(Number) => {
+    await waitFor(2)
+    return Number * Number 
+  }))
+  
+  console.log(squares)
+
+  console.timeEnd('map')
+}
+
+execute()
+
