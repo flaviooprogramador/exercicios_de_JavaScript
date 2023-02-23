f2b341e79ff2ca8dfd618c668436b496b57ac603
Flavio de oliveira <flaviogamer7007@gmail.com>
2023-02-23 20:06:40 -0300

    React

--- React/my-first-app/src/components/planets/planet/index.js
+++ React/my-first-app/src/components/planets/planet/index.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
-import './style.css'
+import GrayImg from '../../shared/gray_img'
 
 const Planet = () => {
   return (
     <div>
         <h4>Mercurio</h4>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-        <img  class="gray-img" src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg"></img>
+        <GrayImg/>
+        
+        
     </div>
     
   );
---
+++ React/my-first-app/src/components/shared/gray_img/index.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import './style.css'
+
+const GrayImg = () => {
+  return (
+    <img  class="gray-img" src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg"
+    ></img>
+  
+  );
+}
+
+export default GrayImg;
