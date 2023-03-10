614ebdf8efe7146d04740e56b6f4d70a14eb733a
Flavio de oliveira <flaviogamer7007@gmail.com>
2023-03-10 15:27:30 -0300

    enums TYPESCRIPT

---
+++ TypeScript/Tipos extras Enums/enum.ts
@@ -0,0 +1,16 @@
+enum Planets {
+  MERCURIO = 'Mercurio',
+  VENUS = 'Venus',
+  TERRA  = 'Terra',
+  MARTE = 'Marte'
+}
+
+Planets.TERRA
+
+
+enum Roles {
+  ADMIN = 'admin',
+  USER = 'User'
+}
+
+Roles.ADMIN
