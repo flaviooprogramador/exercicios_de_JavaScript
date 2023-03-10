49b253e2f3608d3cbd24deba8141b3f7ebd0f1f9
Flavio de oliveira <flaviogamer7007@gmail.com>
2023-03-10 14:12:52 -0300

    type scrip

---
+++ TypeScript/Primeiro code em Typescript/dist/index.js
@@ -0,0 +1,25 @@
+function sendSpace(name, captain) {
+    var spaceship = {
+        name: name,
+        captain: captain,
+        speed: 20,
+        inMission: true,
+        crew: []
+    };
+    alert("A nave ".concat(spaceship.name, " comandada pelo Capit\u00E3o ").concat(spaceship.captain));
+    return spaceship;
+}
+function acelerate(targetSpeed, spaceship) {
+    if (spaceship.speed > targetSpeed) {
+        alert("Reduzindo a velocidade da ".concat(spaceship.name, " para ").concat(targetSpeed, "..."));
+    }
+    else if (spaceship.speed < targetSpeed) {
+        alert(("Aumentando a velocidade da  ".concat(spaceship.name, " para ").concat(targetSpeed, "...")));
+    }
+    else {
+        alert("Mantendo a velocidade da ".concat(spaceship.name, "..."));
+    }
+}
+var spaceshipName = prompt("Insira o nome da nave a ser enviada");
+var spaceshipCaptain = prompt("Insira o nome do capit\u00E3o da nave.");
+sendSpace(spaceshipName, spaceshipCaptain);
