c805e6ce7d93e58a71fdc1aea8dda7e115192031
Flavio de oliveira <flaviogamer7007@gmail.com>
2023-03-10 10:36:52 -0300

    type script

---
+++ TypeScript/Como instalar e usar o Typescript/index.ts
@@ -0,0 +1,18 @@
+const setPilot = (newPilot: string, spaceship: { name?: string; pilot: string; speed?: number; inMission?: boolean; }) => {
+  spaceship.pilot = newPilot;
+}
+
+const accelerate = (targetSpeed: number, spaceship: { name?: string; pilot?: string; speed: any; inMission?: boolean; }) => {
+  spaceship.speed = targetSpeed;
+}
+
+const sendToMission = (spaceship: { name?: string; pilot?: string; speed?: number; inMission: any; }) => {
+  spaceship.inMission = true;
+}
+
+const spaceship = {
+  name: '',
+  pilot: '',
+  speed: 0,
+  inMission: false
+}
