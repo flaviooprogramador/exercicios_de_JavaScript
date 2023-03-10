1a8a2fb1cbd7b4bd7c6c6e6246b3a8857debfdbe
Flavio de oliveira <flaviogamer7007@gmail.com>
2023-03-10 10:36:31 -0300

    Type script

---
+++ TypeScript/Inferência de tipo/index.ts
@@ -0,0 +1,10 @@
+const spaceship = {
+  name: 'Joao',
+  speed: 2
+}
+
+function acelerate(speceship: { name: string; speed: number }, speed: number){
+  spaceship.speed = speed = speed 
+}
+
+acelerate(spaceship, 50)
